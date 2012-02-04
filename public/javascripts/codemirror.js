a0d37295183cf5c8818a99de496b1f46b48eac1a
Adam Butler <adam@littlebigmedia.co.uk>
2012-02-04 14:36:21 +0000

    Lots of changes including add new lightbox

--- public/javascripts/codemirror.js
+++ public/javascripts/codemirror.js
@@ -1,3 +1,9 @@
+/* ######################## */
+/* #                      # */
+/* #  List / Show Editor  # */
+/* #                      # */
+/* ######################## */
+
 $(document).ready(function() {
 	$('.codemirror').each(function(){
 		// Setup CodeMirror
@@ -14,3 +20,48 @@ $(document).ready(function() {
 		updatePreview(a, $(this).closest('.viewport').find('iframe'));
 	});
 });
+
+/* ############################ */
+/* #                          # */
+/* #  Main Editor (lightbox)  # */
+/* #                          # */
+/* ############################ */
+
+$(document).ready(function() {
+	
+	var mainDelay;
+		
+	// Setup CodeMirror
+	var mainEditor = CodeMirror.fromTextArea(document.getElementById('mainCodemirror'), {
+		mode: 'text/html',
+		tabMode: 'indent',
+		lineNumbers: true,
+		onChange: function() {
+			clearTimeout(mainDelay);
+			mainDelay = setTimeout(updateMainPreview, 300);
+		}
+	});
+		
+	function updateMainPreview() {
+		console.log('test')
+		var previewFrame = document.getElementById('mainPreview');
+		var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+		preview.open();
+		preview.write(mainEditor.getValue());
+		preview.close();
+	}
+	
+	$('.lightboxbtn').click(function(){
+		var lightbox = $('#'+$(this).attr('lightbox_id'));
+		
+		$('#'+$(this).attr('lightbox_id')).lightbox_me({
+			centered: true,
+			onLoad: function(){
+				mainEditor.refresh();
+			},
+			closeClick: false,
+			closeSelector: '.btn-close'
+		});
+	});
+});
+
